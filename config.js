255fac005caf5c2160b983bf70c8abfe2b5b7c38
KiyoomasaaNANDE07 <103013721+KiyoomasaaNANDE07@users.noreply.github.com>
2022-09-23 16:28:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 packname = sticker_name
 author = sticker_author
-wm = '© Yunji'
+wm = '© Denis-Bot'
 
 multiplier = 1000 // The higher, The harder levelup
 
